68c781b056592b56bb4c1b7d3fcee91580268718
kishorekumr <kishore.saiprakash@gmail.com>
2024-11-23 01:57:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -257,7 +257,7 @@ def get_mc_history(fincode: str):
     options.add_argument("--disable-gpu")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
     options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(service=service), options=options)
+    driver = webdriver.Chrome(service=service, options=options)
 
     url = "https://www.indiratrade.com"
     driver.get(url)
