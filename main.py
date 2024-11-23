3aa90211a3ec4c96a0a0ee2f2500a21e98c28063
kishorekumr <kishore.saiprakash@gmail.com>
2024-11-23 14:02:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -243,7 +243,7 @@ def get_mc_history(symbol: str):
 
 
 
-@app.get("/history_it2/{exch}/{fincode}" ,response_model=List[Dict[str, Any]]
+@app.get("/history_it2/{exch}/{fincode}" ,response_model=List[Dict[str, Any]])
 async def get_it_history(exch: str, fincode: str):
     url=f'https://www.indiratrade.com/Ajaxpages/companyprofile/CompanyHistoricalVol.aspx?Option={exch}&FinCode={fincode}&fmonth=OCT&fyear=2024&lmonth=NOV&lyear=2024&pageNo=1&PageSize=50'
     client = ScrapingBeeClient(api_key='3YJNM84LOHW51BJ8WMRLMNERNQ4F5U9CLVVNXQ8MUJZA4LL2IWRBIS48QK3PD3WXKQRTS8OIWQ32CJE8')
